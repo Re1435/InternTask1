9e86f286e03cfb2f5ef629071ba0ba328c98b867
Re1435 <reddymahereddy143@gmail.com>
2023-09-12 16:30:33 +0530

    commit the task1

---
+++ app.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { User } from './user.models';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  async createUser(@Body() userDto: User) {
+    return this.appService.createUser(userDto);
+  }
+}
---
+++ app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { User, UserDocument } from './user.models';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class AppService {
+  constructor(@InjectModel('user') private readonly userModel: Model<User>) {}
+
+  async createUser(user: User): Promise<User> {
+    const newUser = new this.userModel(user);
+    return newUser.save();
+  }
+}
